d13b1719a73471c7d98e651406f357fde9e2ab46
OneBlog <wandi99pratama@gmail.com>
2019-02-12 13:46:06 +0700

    Add TabsComponent

---
+++ TabsComponent/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MainApp from "./src/MainApp";
+
+class App extends Component {
+  render() { 
+    return ( 
+      <MainApp/>
+     );
+  }
+}
+ 
+export default App;
