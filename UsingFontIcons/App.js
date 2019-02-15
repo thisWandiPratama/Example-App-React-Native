73ccd864fea21599d612aac10ebd921b963d8e19
OneBlog <wandi99pratama@gmail.com>
2019-02-15 15:13:41 +0700

    Add Using Font Icons

---
+++ UsingFontIcons/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View} from 'react-native'
+import MainApp from './src/MainApp';
+
+class App extends React.Component {
+    render() { 
+        return (  
+            <View>
+                <MainApp/>
+            </View>
+        );
+    }
+}
+ 
+export default App;
